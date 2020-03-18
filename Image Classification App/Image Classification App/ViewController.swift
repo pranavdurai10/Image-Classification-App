eac48bf280b1074a5bc88cdb189658022237d9f6
Pranav Durai <pranavdurai10@gmail.com>
2020-03-18 23:49:41 +0530

    Initial Setup Files

---
+++ Image Classification App/Image Classification App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Classification App
+//
+//  Created by Pranav Durai  on 18/03/20.
+//  Copyright © 2020 Pranav Durai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Image Classification App/Image Classification AppTests/Image_Classification_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Image_Classification_AppTests.swift
+//  Image Classification AppTests
+//
+//  Created by Pranav Durai  on 18/03/20.
+//  Copyright © 2020 Pranav Durai. All rights reserved.
+//
+
+import XCTest
+@testable import Image_Classification_App
+
+class Image_Classification_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
